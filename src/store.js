4d6ec57dd9c68afdc7f090204ee376bbe2cee28f
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-02-04 14:27:29 +0100

    Wire the redux devtools to store

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { createStore } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 
 import { hotColdReducer } from './reducers';
 
-export default createStore(hotColdReducer);
+export default createStore(hotColdReducer, devToolsEnhancer());
