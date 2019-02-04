748d97a1fb2fbdc27a4cac0fbd9de0891109f470
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-02-04 14:15:36 +0100

    Fix Reset code style and add propTypes validation

--- src/components/Reset/Reset.js
+++ src/components/Reset/Reset.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import { Button } from '@smooth-ui/core-sc';
+import PropTypes from 'prop-types';
 
-const Reset = ({resetGame}) => (
+const Reset = ({ resetGame }) => (
   <div>
-    <Button variant="danger" width={1} mt={10} minHeight={40} fontSize={22} type="button" onClick={resetGame}>
-      Reset Game
+    <Button 
+      variant="danger" 
+      width={1} 
+      mt={10} 
+      minHeight={40} 
+      fontSize={22} 
+      type="button" 
+      onClick={resetGame}
+    >
+        Reset Game
     </Button>
   </div>
 );
+
+Reset.propTypes = {
+  resetGame: PropTypes.func.isRequired,
+}
 
 export default Reset;
