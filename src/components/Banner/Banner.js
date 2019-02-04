2a383be3d8858309484479d477cfc51966a15aa5
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2019-02-04 13:53:56 +0100

    Fix Banner code style and memoize componeny

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Typography } from '@smooth-ui/core-sc';
 
-const Banner = () => (
-  <Typography variant="h1" textAlign="center" m={0}>
+const Banner = React.memo(() => (
+  <Typography 
+    variant="h1" 
+    textAlign="center" 
+    m={0}
+  >
     HOT or COLD
   </Typography>
-);
+));
 
 export default Banner;
